6764db1ce3adc87fcad36279b40287975679aaee
cqxg <cqxg@yandex.ru>
2019-06-27 05:15:04 +0300

    feat: add set color & bucket tools

--- FAE/src/js/AppController/AppController.js
+++ FAE/src/js/AppController/AppController.js
@@ -8,22 +8,27 @@ export default class AppController {
     this.do = 'pen';
     this.was = 'pen';
     this.tools = document.querySelector('.tools');
+    document.getElementById('get_color').addEventListener('change', e => this.view.changeColor(e));
     this.player = document.querySelector('.play-wrapper');
     this.framsControl = document.querySelector('.button-wrapper');
     this.transformControl = document.querySelector('.transform-tool');
-    
+
     document.getElementById('get_color').addEventListener('change', e => this.view.changeColor(e));
 
     this.tools.addEventListener('click', (e) => {
       this.done(e);
       if (this.do === 'pen' || this.do === 'eraser' || this.do === 'line' || this.do === 'circle' || this.do === 'stroke-circle' || this.do === 'stroke-rectngle' || this.do === 'rectngle') {
         document.getElementById('range-wrapper').style.display = 'block';
-        // eslint-disable-next-line no-shadow
         document.getElementById('width').addEventListener('change', e => this.view.changeWidth(e));
       } else {
         document.getElementById('range-wrapper').style.display = 'none';
       }
     });
+
+    this.view.canvas.addEventListener('click', (e) => {
+      if (this.do === 'bucket-full') this.view.bucketFull((this.view.color));
+      if (this.do === 'bucket') this.view.bucket(e, (this.view.color))
+    });;
   }
 }
 
--- FAE/src/js/AppView/AppView.js
+++ FAE/src/js/AppView/AppView.js
@@ -37,6 +37,38 @@ export default class AppView {
     this.context.globalCompositeOperation = 'source-over';
   }
   //-------------------
+  
+  // color (change, select, bucket, fullbucket, transparency)
+  changeColor(e) {
+    this.color = e.target.value;
+  }
 
+  selectColor(e) {
+    const xCanvas = e.pageX - this.canvas_cont.offsetLeft;
+    const yCanvas = e.pageY - this.canvas_cont.offsetTop;
+    this.color = `rgba(${this.context.getImageData(xCanvas, yCanvas, 1, 1).data.join(', ')})`;
+    return this.context.getImageData(xCanvas, yCanvas, 1, 1).data;
+  }
+
+  bucketFull() {
+    this.backgroundColor.style.backgroundColor = this.color;
+    this.backroundcolor = this.backgroundColor.style.backgroundColor;
+  }
+
+  bucket(e, color_rgb = { b: 0, g: 0, r: 0 }) {
+    const xCanvas = e.pageX - this.canvas_cont.offsetLeft;
+    const yCanvas = e.pageY - this.canvas_cont.offsetTop;
+    const c = this.context.getImageData(xCanvas, yCanvas, 1, 1).data;
+    const imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    const { data } = imageData;
+    for (let i = 0; i < data.length; i += 4) {
+      if (data[i] === c[0] && data[i + 1] === c[1] && data[i + 2] === c[2]) {
+        data[i] = color_rgb.r;
+        data[i + 1] = color_rgb.g;
+        data[i + 2] = color_rgb.b;
+      }
+    }
+    this.context.putImageData(imageData, 0, 0);
+  }
 
 }
