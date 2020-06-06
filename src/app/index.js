49b2d163eb042fb96f6c6aaa030e44294d7c6eb4
unknown <cqxg@yandex.ru>
2020-06-06 13:58:12 +0300

    feat: add eslint

---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+class TestClass {
+  constructor() {
+    const msg = 'Using ES2015+ syntax';
+    console.log(msg);
+  }
+}
+const test = new TestClass();
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,35 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: __dirname + "/src/app/index.js",
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + "/src/public/index.html",
-            inject: 'body'
-        })
+  entry: `${__dirname}/src/app/index.js`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: [
+          /node_modules/,
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    devServer: {
-        contentBase: './src/public',
-        port: 7700,
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/public/index.html`,
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    contentBase: './src/public',
+    port: 7700,
+  },
 };
