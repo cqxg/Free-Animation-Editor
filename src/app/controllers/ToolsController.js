6ba02fa0883e58094f5347d7460cb54b16c6e6f0
unknown <cqxg@yandex.ru>
2020-07-30 01:16:29 +0300

    feat: add pen tool workpiece

--- src/app/controllers/ToolsController.js
+++ src/app/controllers/ToolsController.js
@@ -1,42 +1,79 @@
 const controller = () => {
     const tools = document.querySelector('.tools');
+    const canvas = document.querySelector('.canvas__field');
+    const ctx = canvas.getContext('2d');
 
-    const state = {
-        currentTool: '',
-        currentColor: '',
+    // resizing canvas.height / width
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+
+    // const state = {
+    //     painting: false,
+    //     currentTool: '',
+    //     currentColor: '',
+    // };
+
+    let painting;
+
+    // let { painting, currentTool, currentColor } = state;
+
+    // const toolIdentifier = (e) => {
+    //     currentTool = e.target.className;
+    //     switch (currentTool) {
+    //         case 'pen':
+    //             console.log('u want pen')
+    //             break;
+    //         case 'line':
+    //             console.log('u want line')
+    //             break;
+    //         case 'bucket':
+    //             console.log('u want bucket')
+    //             break;
+    //         case 'eraser':
+    //             console.log('u erase')
+    //             break;
+    //         case 'circle':
+    //             console.log('u want circle')
+    //             break;
+    //         case 'rect':
+    //             console.log('u want rect')
+    //             break;
+    //         case 'dropper':
+    //             console.log('u want dropper')
+    //             break;
+    //         case 'mover':
+    //             console.log('u want mover')
+    //             break;
+    //     }
+    // };
+
+    const startPosition = (e) => {
+        painting = true;
+        draw(e);
     };
 
-    const toolIdentifier = (e) => {
-        state.currentTool = e.target.className;
-        switch (state.currentTool) {
-            case 'pen':
-                console.log('u want pen')
-                break;
-            case 'line':
-                console.log('u want line')
-                break;
-            case 'bucket':
-                console.log('u want bucket')
-                break;
-            case 'eraser':
-                console.log('u erase')
-                break;
-            case 'circle':
-                console.log('u want circle')
-                break;
-            case 'rect':
-                console.log('u want rect')
-                break;
-            case 'dropper':
-                console.log('u want dropper')
-                break;
-            case 'mover':
-                console.log('u want mover')
-                break;
-        }
+    const finishedPosition = () => {
+        painting = false;
+        ctx.beginPath();
     };
 
-    tools.addEventListener('click', (e) => toolIdentifier(e))
+    const draw = (e) => {
+        if (!painting) return;
+
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'red';
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY)
+    };
+
+    // tools.addEventListener('click', (e) => toolIdentifier(e));
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mousedown', finishedPosition);
+    canvas.addEventListener('mousemove', draw);
+
 };
 
 document.addEventListener('DOMContentLoaded', controller);
