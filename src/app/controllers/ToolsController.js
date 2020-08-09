f16d1ae66968b4955e02a7ae04e72935f675e91d
unknown <cqxg@yandex.ru>
2020-08-10 00:46:32 +0300

    feat: start work with frames

--- src/app/controllers/ToolsController.js
+++ src/app/controllers/ToolsController.js
@@ -2,6 +2,17 @@ import { pen } from './tools/Pen';
 
 const controller = () => {
     const tools = document.querySelector('.tools');
+    const addFrameBtn = document.querySelector('.frames__add');
+
+    const frames = [];
+    const framesTwo = [];
+    const framesWrapper = document.querySelector('.frames-wrapper');
+    const frameTemplate = document.querySelector('#frame-template');
+
+    const canvas = document.querySelector('.canvas__field');
+    const ctx = canvas.getContext('2d');
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
 
     const toolIdentifier = (e) => {
         switch (e.target.className) {
@@ -33,7 +44,68 @@ const controller = () => {
         }
     };
 
+    const clear = () => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, 800, 600);
+    };
+
+    const createFrame = (frameParams) => {
+        const { url, id } = frameParams;
+
+        const fragment = document.createDocumentFragment();
+
+        const newFrame = document.importNode(frameTemplate.content, true);
+
+        const frameImage = newFrame.querySelector('.frame-image');
+        const frame = newFrame.querySelector('.frame');
+        frame.id = `${id}`;
+        frameImage.src = url;
+        frameImage.id = `${id}`;
+
+
+        const frameDeleteHandler = (e) => {
+            const elem = e.target;
+            const num = (elem.classList.contains('button-delete')) ? elem.parentElement.id : elem.parentElement.parentElement.id;
+
+            framesTwo.splice(num, 1);
+            frames.splice(num, 1);
+            frame.remove(e.target);
+        }
+
+        const frameCopyHandler = (e) => {
+            const elem = e.target;
+            const num = (elem.classList.contains('button-copy')) ? elem.parentElement.id : elem.parentElement.parentElement.id;
+            frameDraw(num);
+        }
+
+        const frameDelete = newFrame.querySelector('.button-delete');
+        frameDelete.addEventListener('click', frameDeleteHandler);
+
+        const frameCopy = newFrame.querySelector('.button-copy');
+        frameCopy.addEventListener('click', frameCopyHandler);
+
+        fragment.appendChild(newFrame);
+        return fragment;
+    }
+
+    const addFrame = () => {
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        framesTwo.push(imageData);
+
+        const dataURL = canvas.toDataURL();
+        frames.push(dataURL);
+
+        const frameId = frames.length - 1;
+
+        const fragment = createFrame({ url: dataURL, id: frameId });
+        framesWrapper.appendChild(fragment);
+        clear();
+
+        console.log('IMAGE DATA', imageData);
+    };
+
     tools.addEventListener('click', (e) => toolIdentifier(e));
+    addFrameBtn.addEventListener('click', (e) => addFrame(e));
 };
 
 document.addEventListener('DOMContentLoaded', controller);
