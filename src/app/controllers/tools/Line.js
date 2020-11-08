02c161bdb87e5109a95c0b77f3f6f1741677932a
unknown <cqxg@yandex.ru>
2020-11-08 19:39:30 +0300

    fix: line

--- src/app/controllers/tools/Line.js
+++ src/app/controllers/tools/Line.js
@@ -1,18 +1,25 @@
 const line = (canvas, ctx, color, lineWidth) => {
-  
+  let painting;
+
   ctx.lineCap = 'round';
   ctx.lineJoin = 'round';
   ctx.strokeStyle = color;
   ctx.lineWidth = lineWidth;
 
   canvas.onmousedown = (e) => {
+    painting = true;
+
     canvas.onmousemove = (e) => {
+      if (!painting) return;
+      
       ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     };
 
     canvas.onmouseup = () => {
+      painting = false;
       ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
       ctx.stroke();
+      ctx.beginPath();
     };
   };
 };
